51f7da52251f1a51ea07b0f17a6def996ba504f3
Sumit Kumar Yadav <47476553+sumit18cs@users.noreply.github.com>
2021-08-23 12:42:15 +0530

    Create Different Bits Sum Pairwise.cpp

---
+++ Bit Manipulation/Different Bits Sum Pairwise.cpp
@@ -0,0 +1,18 @@
+int Solution::cntBits(vector<int> &v) {
+    long long int i,n,ans=0,a,j;
+    n=v.size();
+    long long int x[34];
+    for(i=0;i<33;i++){x[i]=0;}
+    for(i=0;i<n;i++){
+        a=v[i];
+        for(j=0;j<32;j++){
+            if(a&(1<<j)){x[j]++;}
+        }
+    }
+    for(i=0;i<32;i++){
+        // cout<<x[i]<<" ";
+        ans+=(x[i])*(n-x[i]);
+        ans%=(1000000007);
+    }
+    return (2*ans)%(1000000007);
+}
