ad8a2655ff12a8d4c6b8fce9f0e49b452c1e5586
Sumit Kumar Yadav <47476553+sumit18cs@users.noreply.github.com>
2021-08-23 13:02:11 +0530

    Create Subsets II.cpp

---
+++ Backtracking/Subsets II.cpp
@@ -0,0 +1,21 @@
+
+#define pb push_back
+vector<vector<int>> Solution::subsetsWithDup(vector<int>& v) {
+    vector<vector<int> >ans;
+    set<vector<int> >s;
+    int i,n=v.size(),j;
+    for(i=0;i<(1<<n);i++){
+        vector<int>a;
+        for(j=0;j<n;j++){
+            if(i&(1<<j)){
+                a.pb(v[j]);
+            }
+        }
+        sort(a.begin(),a.end());
+        s.insert(a);
+    }
+    for(auto i:s){
+        ans.pb(i);
+    }
+    return ans;
+}
